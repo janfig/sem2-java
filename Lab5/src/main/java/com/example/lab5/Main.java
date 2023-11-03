afcd186a859c083ef809837325ba53b92e3fcbc6
user.email <user.email>
2023-11-03 18:51:09 +0100

    feat: next change

--- Lab5/src/main/java/com/example/lab5/Main.java
+++ Lab5/src/main/java/com/example/lab5/Main.java
@@ -1,4 +1,16 @@
 package com.example.lab5;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
+@ComponentScan
+@EnableJpaRepositories
+@EntityScan
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
