222eb5f2b91bcb53656b591dea3329b6cdc5af25
Jan Figura <janfig27@gmail.com>
2023-11-10 15:39:43 +0100

    feat: task 6.5 and task 6.6

--- Lab6/src/main/java/com/example/lab6/controllers/UserController.java
+++ Lab6/src/main/java/com/example/lab6/controllers/UserController.java
@@ -3,13 +3,18 @@ package com.example.lab6.controllers;
 import com.example.lab6.dao.UserDao;
 import com.example.lab6.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -23,25 +28,73 @@ public class UserController {
     public String loginPage() {
         return "login";
     }
+
     @GetMapping("/register")
     public String registerPage(Model m) {
         m.addAttribute("user", new User());
         return "register";
     }
+
+//    @PostMapping("/register")
+//    public String registerPagePOST(@ModelAttribute User user) {
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        dao.save(user);
+//        return "redirect:/login";
+//    }
+
     @PostMapping("/register")
-    public String registerPagePOST(@ModelAttribute User user) {
+    public String registerPagePOST(@Valid User user, BindingResult binding) {
+        if (binding.hasErrors()) {
+            return "register";
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         dao.save(user);
         return "redirect:/login";
     }
+
     @GetMapping("/profile")
     public String profilePage(Model m, Principal principal) {
         m.addAttribute("user", dao.findByLogin(principal.getName()));
         return "profile";
     }
+
     @GetMapping("/users")
     public String users(Model m) {
         m.addAttribute("users", dao.findAll());
         return "users";
+    }
+
+    @GetMapping("/edit")
+    public String editPage(Model m, Principal principal) {
+        User user = dao.findByLogin(principal.getName());
+        user.setPassword("");
+        m.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String editPagePOST(@ModelAttribute User user, Principal p) throws Exception {
+        String newPassword = user.getPassword();
+        if(newPassword.isBlank()) {
+            String existingPassword = dao.findByLogin(p.getName()).getPassword();
+            user.setPassword(existingPassword);
+        } else {
+            user.setPassword(passwordEncoder.encode(newPassword));
+        }
+
+        User existingUser = dao.findByLogin(user.getLogin());
+        if(existingUser != null && p.getName().equals(existingUser.getName()))
+            throw new Exception("Login " + user.getLogin() + "is taken.");
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        dao.save(user);
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/delete")
+    public String editPagePOST(Principal principal) {
+        User user = dao.findByLogin(principal.getName());
+        dao.delete(user);
+        return "redirect:/logout";
     }
 }
--- Lab6/src/main/java/com/example/lab6/entity/User.java
+++ Lab6/src/main/java/com/example/lab6/entity/User.java
@@ -3,6 +3,8 @@ package com.example.lab6.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "Users")
@@ -10,14 +12,22 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer userid;
+    private Integer userId;
 
+    @NotNull
+    @Size(min = 1, max = 30, message = "Imię musi mieć pomiędzy 1 a 30 znaków")
     private String name;
 
+    @NotNull
+    @Size(min = 1, max = 50, message = "Nazwisko musi mieć pomiędzy 3 a 30 znaków")
     private String surname;
 
+    @NotNull
+    @Size(min = 3, max = 30, message = "Login music mieć pomiędzy 3 a 30 znaków")
     private String login;
 
+    @NotNull
+    @Size(min = 6, message = "Hasło musi mieć co najmniej 6 znaków")
     private String password;
 
     public User() {
